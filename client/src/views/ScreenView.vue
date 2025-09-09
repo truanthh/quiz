a916151cbcda0e3f652a7ecbb742f13e18da3fc2
truanthh <aquapyr@gmail.com>
2025-09-09 22:58:11 +0300

    trying to use html5 audioelement

--- client/src/views/ScreenView.vue
+++ client/src/views/ScreenView.vue
@@ -1,103 +1,46 @@
 <script setup>
-import { ref, onMounted, onUnmounted, watch } from "vue";
+import { ref, useTemplateRef, onMounted } from "vue";
 
-const props = defineProps({
-  // manifestUrl: {
-  //   type: String,
-  //   required: true,
-  // },
-  licenseServer: {
-    type: String,
-    required: true,
-  },
-  posterUrl: {
-    type: String,
-    required: false,
-    default: "",
-  },
-});
+const audioContext = new AudioContext();
+const audioPlayer = useTemplateRef("audioPlayer");
 
-const videoContainer = ref(null);
-const videoPlayer = ref(null);
-let player = null;
-let ui = null;
+let track = ref(null);
 
-const mediaUrl = ref("C:/Users/user/Desktop/bla/quiz/client/public/zvezda.mp3");
-
-const onError = (error) => {
-  console.error("Error code", error.code, "object", error);
-};
-
-const initializePlayer = async () => {
-  try {
-    // Динамический импорт Shaka Player
-    const shaka = await import("shaka-player/dist/shaka-player.ui.js");
-
-    if (!videoPlayer.value) return;
-
-    player = new shaka.Player(videoPlayer.value);
-    ui = new shaka.ui.Overlay(player, videoContainer.value, videoPlayer.value);
-    ui.getControls();
-
-    player.configure({
-      drm: {
-        servers: { "com.widevine.alpha": props.licenseServer },
-      },
-    });
-
-    await player.load(mediaUrl.value);
-    console.log("The video has now been loaded!");
-  } catch (error) {
-    onError(error);
+function playPause() {
+  console.log("trying to play audio!");
+  console.log(audioContext);
+  console.log(audioContext.state);
+  if (audioContext.state === "suspended") {
+    audioContext.resume();
+    audioPlayer.play();
   }
-};
+}
 
-const cleanupPlayer = () => {
-  if (player) {
-    player.destroy();
-    player = null;
-  }
-  if (ui) {
-    ui.destroy();
-    ui = null;
-  }
-};
-
-// Инициализация при монтировании компонента
 onMounted(() => {
-  initializePlayer();
+  track.value = audioContext.createMediaElementSource(audioPlayer.value);
+  // audioPlayer.play();
+  track.value.connect(audioContext.destination);
+  // console.log(track.value);
 });
-
-// Очистка при размонтировании компонента
-onUnmounted(() => {
-  cleanupPlayer();
-});
-
-// Обновление при изменении пропсов
-watch(
-  () => [props.manifestUrl, props.licenseServer],
-  () => {
-    cleanupPlayer();
-    initializePlayer();
-  },
-);
 </script>
 
 <template>
-  <div ref="videoContainer" class="shadow-lg mx-auto max-w-full size">
-    <video
-      id="video"
-      ref="videoPlayer"
-      class="w-full h-full"
-      :poster="posterUrl"
-    ></video>
-  </div>
+  <div>play plsss</div>
+  <button
+    data-playing="false"
+    role="switch"
+    aria-checked="false"
+    @click="playPause"
+    @keyup.enter="playPause"
+  >
+    <span>Play/Pause</span>
+  </button>
+  <audio
+    src="C:/Users/user/Desktop/bla/quiz/client/public/zvezda.mp3"
+    ref="audioPlayer"
+  ></audio>
 </template>
 
 <style>
-@import "../../node_modules/shaka-player/dist/controls.css"; /* Shaka player CSS import */
-
-.size {
-  width: 800px;
-}
+/* sd */
 </style>
