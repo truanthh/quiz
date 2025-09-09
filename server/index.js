bc1282506d776a428c8bc64497f8b5f6cbba3eda
truanthh <aquapyr@gmail.com>
2025-09-09 15:07:21 +0300

    bla

--- server/index.js
+++ server/index.js
@@ -1,24 +1,66 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+// server/index.js
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-// Раздаем статические файлы
-app.use(express.static('public'));
-
-// Запускаем сервер на порту 3000
-server.listen(3000, '0.0.0.0', () => { // '0.0.0.0' - слушаем все интерфейсы
-  console.log('Сервер запущен!');
-  console.log('Подключайтесь по адресу: http://ВАШ_IP:3000');
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Для разработки
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log('Новый пользователь подключился:', socket.id);
-    
-    socket.on('disconnect', () => {
-        console.log('Пользователь отключился:', socket.id);
-    });
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+// app.use(express.static("public"));
+
+// Обработка подключений
+io.on("connection", (socket) => {
+  console.log("Подключился:", socket.id);
+
+  socket.emit("connection-established", {
+    message: "Подключение успешно",
+    clientId: socket.id,
+    connectedAt: new Date().toLocaleString(),
+  });
+
+  // Присоединение к игре
+  socket.on("join-game", (data) => {
+    // socket.join(data.gameId);
+    // socket.to(data.gameId).emit("player-joined", data.playerName);
+  });
+
+  // Отправка ответа
+  socket.on("send-msg", (data) => {
+    // Сохраняем ответ и рассылаем остальным
+    console.log(`${data.timestamp}: ${data.msg}`);
+    // io.to(data.gameId).emit("new-answer", {
+    //   player: data.playerName,
+    //   answer: data.answer,
+    // });
+  });
+
+  // Запуск таймера
+  socket.on("start-timer", (data) => {
+    io.to(data.gameId).emit("timer-start", data.duration);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Отключился:", socket.id);
+  });
+});
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Сервер запущен на порту 3000");
 });
